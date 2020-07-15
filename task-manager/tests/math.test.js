d664a14dcb4fd1364ee21852f24411c5af99d381
prithdamyth <pvelpur@purdue.edu>
2020-07-15 18:07:31 -0500

    learned testing using JEST

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,4 +1,4 @@
-const {calculateTip, fahrenheitToCelsius, celsiusToFahrenheit} = require('../src/math')
+const {calculateTip, fahrenheitToCelsius, celsiusToFahrenheit, add} = require('../src/math')
 
 test('Should calculate total with tip', () => {
     const total = calculateTip(10, 0.3)
@@ -21,4 +21,23 @@ test('Should convert 32 F to 0 C', () => {
 
 test('Should convert 0 C to 32 F', () => {
     expect(celsiusToFahrenheit(0)).toBe(32)
+})
+
+// test('Async test demo', (done) => {
+//     setTimeout(() => {
+//         expect(1).toBe(2)
+//         done()
+//     }, 2000)
+// })
+
+test('Should add 2 numbers', (done) => {
+    add(2, 3).then((sum) => {
+        expect(sum).toBe(5)
+        done()
+    })
+})
+
+test('Should add 2 number async/await', async () => {
+    const sum = await add(10, 22)
+    expect(sum).toBe(32)
 })
