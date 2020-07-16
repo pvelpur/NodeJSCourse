cfc0da6091de74b2bf83f3975c4f84e90367459a
prithdamyth <pvelpur@purdue.edu>
2020-07-16 01:34:26 -0500

    broadcast => send to everyone but client that initiated

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -13,15 +13,18 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-let msg = "Welcome"
-
 io.on('connection', (socket) => {
     //socket is an object that contains info about the new connection
     console.log('new websocket connection')
-    socket.emit('message', msg)
+    socket.emit('message', "Welcome!")
+    socket.broadcast.emit('message', 'A new user has joined!')
 
     socket.on('sendMessage', (message) => {
         io.emit('message', message)
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has been disconnected')
     })
 
 })
